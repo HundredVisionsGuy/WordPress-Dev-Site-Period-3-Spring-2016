379177d742244ddca0596038c7e9d82434ef2739
hundredvisionsguy <winikkc@hsd.k12.or.us>
2016-05-26 12:57:28 -0700

    Added Bootstrap Grid to 404 & archive

--- wp-content/themes/HTML5-Reset-Child/404.php
+++ wp-content/themes/HTML5-Reset-Child/404.php
@@ -8,7 +8,7 @@
 <!-- 404.php -->
 <div class="row">
 	<section class="col-md-8">
-		<h2><?php _e('Error 404 - Nothing to see here...Move along!','html5reset'); ?></h2>
+		<h2><?php _e('Error 404 - Page Not Found','html5reset'); ?></h2>
 	</section>
 	<aside class="col-md-4">
 		<?php get_sidebar(); ?>
